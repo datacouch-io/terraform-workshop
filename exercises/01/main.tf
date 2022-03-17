8de33dc6b22cf990f0eb2ad569ed9c0616e41708
sridhar09 <sridhar.mundra@gmail.com>
2022-03-17 14:41:33 +0530

    Update main.tf

--- exercises/01/main.tf
+++ exercises/01/main.tf
@@ -4,7 +4,7 @@
 # This provider supports setting the provider version, AWS credentials as well as the region.
 # It can also pull credentials and the region to use from environment variables, which we have set, so we'll use those
 provider "aws" {
-  version = "~> 2.0"
+  version = "~> 3.0"
 }
 
 # declare a resource stanza so we can create something.
